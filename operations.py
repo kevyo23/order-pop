bf180dcca402a3adafd751c120bdcd3c61b4d578
Kevin Y <kevineyu@gmail.com>
2016-12-24 15:35:03 +1100

    Implement colorama functionality, small fixes

--- operations.py
+++ operations.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
-import os
+'''
+
+order-pop:
+A simple Python game where users reorder given numbers to find the correct result.
+Operations locations are fixed - users fit the given numbers around the operations
+Written by Kevin Yu on 24/12/2016
+
+'''
+
 import random
-import numpy
-import operator
+from colorama import init, Fore
+init()
 
 def create_list():
     list = []
@@ -13,42 +21,38 @@ def create_list():
 
 def create_value(shuff, ops):
     return eval(str(shuff[0]) + ops[0] + str(shuff[1]) + ops[1] + str(shuff[2]) + ops[2] + str(shuff[3]))
-    '''
-    ops = {'+':operator.add,
-           '-':operator.sub,
-           '*':operator.mul,
-           '/':operator.truediv}
-    '''
 
 def main():
-
     win = False
     attempts = 0
     nums = create_list()
     shuff = list(nums)
-    random.shuffle(shuff)
     ops = list('+-*')
+
+    random.shuffle(shuff)
     random.shuffle(ops)
     value = create_value(shuff, ops)
-    print "Welcome to guess the order\n==========================="
-    #print "\nI'm thinking of a number, you have to guess what it is.\n"
-    print "Here are a list of random numbers"
-    print ' '.join(map(str, nums))
-    print "Here are a list of operations, in order"
-    print ' '.join(ops)
-    print "Please enter the numbers in correct order to give ", value
-    print "Note operation priority i.e. * before + or -"
+
+    print "Welcome to order-pop! \n===========================\n",
+    print "Here are a list of random numbers\n", (Fore.BLUE + ' '.join(map(str, nums))), Fore.RESET
+    print "Here are a list of operations, in order\n", (Fore.BLUE + ' '.join(ops)), Fore.RESET
+    print "Please enter the numbers in correct order to give", (Fore.BLUE + str(value)), Fore.RESET
+    print "Note operation priority i.e. * before + or -\n",
 
     while win != True:
         if attempts > 10:
             print "Game over, correct order is ", ' '.join(map(str, shuff))
             return
-        user = map(int, raw_input().split())
-        attempts = attempts + 1
+        attempts += 1
+        try:
+            user = map(int, raw_input().split())
+        except ValueError:
+            print (Fore.RED + "Please input the correct numbers provided only!"), Fore.RESET
+            continue
         if user.__len__() == shuff.__len__():
             uservalue = create_value(user,ops)
         else:
-            print user.__len__(), "input values detected. Trying to be smart, are we?"
+            print (Fore.RED + str(user.__len__())), "input values detected. Trying to be smart, are we?", Fore.RESET
             continue
         shuff.sort()
         user.sort()
@@ -57,7 +61,7 @@ def main():
                 print "===========================\nWell done, that is correct!"
                 win = True;
             else:
-                print "Incorrect, your order gives ",uservalue
+                print (Fore.RED + "Incorrect, your order gives"), uservalue, Fore.RESET
         else:
-            print "Incorrect input detected. Please use values provided"
+            print (Fore.RED + "Incorrect input detected. Please use values provided."), Fore.RESET
 main()
