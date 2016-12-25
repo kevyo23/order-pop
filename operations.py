e9a2dba7c1b62c29794fccc10ad9cb6593676376
Kevin Y <kevineyu@gmail.com>
2016-12-26 10:30:04 +1100

    Add dynamic difficulty (list size) given by user

--- operations.py
+++ operations.py
@@ -13,47 +13,103 @@ import random
 from colorama import init, Fore
 init()
 
-def create_list():
+# create_list generates a list of random integers
+def create_list(size):
     list = []
-    for i in range(4):
-        list.append(random.randint(1,10))
+    for i in range(size):
+        list.append(random.randint(1,20))
     return list
 
-def create_value(shuff, ops):
-    return eval(str(shuff[0]) + ops[0] + str(shuff[1]) + ops[1] + str(shuff[2]) + ops[2] + str(shuff[3]))
+# create_list generates a list of random operations
+def create_ops(size):
+    sampops = list('*-+')
+    olist = []
+    for i in range(size - 1):
+        olist.append(sampops[i%3])
+    return olist
 
+
+# create_value calculates the result based off the numbers and operations in input lists
+def create_value(shuff, ops, size):
+    string = ""
+    for i in range(size - 1):
+        string = string + str(shuff[i]) + ops[i]
+    string = string + str(shuff[size - 1])
+    return eval(string)
+
+'''
+def create_value(shuff, ops, size):
+    string = []
+    for i in range(size - 1):
+        string.append(str(shuff[i]))
+        string.append(ops[i])
+    string.append(str(shuff[size - 1]))
+    return eval(''.join(string))
+'''
+
+
+# main function
 def main():
+    print "Welcome to order-pop! \n==========================="
+    print "Please enter your level of difficulty (3 - 8): "
+    # variable initialisation
+    try:
+        usersize = int(raw_input())
+    except ValueError:
+        # a non-integer input
+        print (Fore.RED + "Please input an integer from 3 - 8 only!")
+        print "Reverting to default size 5", Fore.RESET
+        usersize = 5
+    if usersize < 3 or usersize > 8:
+        print (Fore.RED + "Please input an integer from 3 - 8 only!")
+        print "Reverting to default size 5", Fore.RESET
+        usersize = 5
     win = False
     attempts = 0
-    nums = create_list()
+    nums = create_list(usersize)
+    ops = create_ops(usersize)
     shuff = list(nums)
-    ops = list('+-*')
 
+    # randomise order of numbers and operations
     random.shuffle(shuff)
     random.shuffle(ops)
-    value = create_value(shuff, ops)
 
-    print "Welcome to order-pop! \n===========================\n",
+    # find correct value
+    value = create_value(shuff, ops, usersize)
+
     print "Here are a list of random numbers\n", (Fore.BLUE + ' '.join(map(str, nums))), Fore.RESET
     print "Here are a list of operations, in order\n", (Fore.BLUE + ' '.join(ops)), Fore.RESET
     print "Please enter the numbers in correct order to give", (Fore.BLUE + str(value)), Fore.RESET
     print "Note operation priority i.e. * before + or -\n",
 
+    # determine if user evaluates correct value
     while win != True:
-        if attempts > 10:
+        # max of 10 attempts
+        if attempts >= 10:
             print "Game over, correct order is ", ' '.join(map(str, shuff))
             return
         attempts += 1
+
+        if attempts == 8 and usersize > 5:
+            print "Here's a hint (first half of the solution) ", ' '.join(map(str,shuff[:(len(shuff)/2)]))
+
+        # user input
         try:
             user = map(int, raw_input().split())
         except ValueError:
+            # a non-integer input
             print (Fore.RED + "Please input the correct numbers provided only!"), Fore.RESET
             continue
         if user.__len__() == shuff.__len__():
-            uservalue = create_value(user,ops)
+            # determine user's value
+            uservalue = create_value(user, ops, usersize)
         else:
+            # more or less numbers input
             print (Fore.RED + str(user.__len__())), "input values detected. Trying to be smart, are we?", Fore.RESET
             continue
+
+        # correct number of integer inputs
+        # sort and compare number lists
         shuff.sort()
         user.sort()
         if shuff == user:
