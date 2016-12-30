6096615087bc0c632b7d72a0e1716aa15ef64a74
Kevin Y <kevineyu@gmail.com>
2016-12-30 16:43:47 +1100

    Cleaner user experience, improve algo efficiency

--- invent-a-story.py
+++ invent-a-story.py
@@ -3,21 +3,16 @@
 # invent-a-story.py - exploring python dictionaries and collection module
 # Kevin Yu on 30/12/2016
 
-#TODO
-
-# remove items
-# clear dictionary
-# not using lists
-
 import collections
 
 # syntax to display a dictionary
 def display_dictionary(dictionary, title, is_formatted):
-    if is_formatted: formatted_display(dictionary, title)
-    else: standard_display(dictionary, title)
+    sorted_dict = sort_inventory(dictionary)
+    if is_formatted: formatted_display(sorted_dict, title)
+    else: standard_display(sorted_dict, title)
     print('\n')
 
-# without allignment
+# from: display_dictionary -> without allignment
 def standard_display(dictionary, title):
     print(title)
     item_total = 0
@@ -26,11 +21,12 @@ def standard_display(dictionary, title):
         item_total += v
     print('Total number of items: ' + str(item_total))
 
-# with allignment
+# from: display_dictionary -> with allignment
 def formatted_display(dictionary, title):
     item_total = 0
     L = 24
     R = 6
+    # dynamic adjustment according to user item input
     for k, v in dictionary.items():
         if len(k) > L:
             L = len(k)
@@ -42,85 +38,77 @@ def formatted_display(dictionary, title):
         print(k.ljust(L, '.') + (str(v).rjust(R)))
     print('Total number of items: '.ljust(L, ' ') + str(item_total).rjust(R))
     
-
-# add a list of items into an existing inventory
-def add_to_inventory(inventory, added_items):
-    for i in range(len(added_items)):
-        inventory.setdefault(added_items[i], 0)
-        inventory[added_items[i]] += 1
-    return sort_inventory(inventory)
-
-# transform an unordered list of items to a new inventory
-def normalise_list(added_items):
-    new_inventory = {}
-    for i in range(len(added_items)):
-        new_inventory.setdefault(added_items[i], 0)
-        new_inventory[added_items[i]] += 1
-    return sort_inventory(new_inventory)
-
-# combine secondary inventory into the primary
-def combine_inventory(primary_inv, secondary_inv):
-    for k, v in secondary_inv.items():
-        primary_inv.setdefault(k, 0)
-        primary_inv[k] += v
-    return sort_inventory(primary_inv)
+# add or take an item in an existing inventory
+# item_data is a list of ['@command', 'item name', number of items]
+def edit_item(inventory, item_data, is_add):
+    item_value = inventory.setdefault(item_data[1], 0)
+    if int(item_data[2]) > 1000000:
+        print('Number entered exceeds limit')
+    elif len(item_data[1]) > 72:
+        print('Item name cannnot be longer than 72 characters')
+    else:
+        if is_add:
+            if item_value == 0:
+                print(item_data[1] + ' has been added')
+            item_value += int(item_data[2])
+        else:
+            if item_value == 0:
+                print('Can\'t take from an item not in the inventory!')
+                return inventory
+            item_value -= int(item_data[2])
+        inventory[item_data[1]] = item_value
+        if item_value <= 0:
+            print(item_data[1] + ' has been removed')
+            del inventory[item_data[1]]
+    return inventory
 
 # rearrange dictionary in reverse sorted order (greatest item value first)
 def sort_inventory(inventory):
     return collections.OrderedDict(sorted(inventory.items(), key = lambda t: t[1], reverse=True))
 
-def collect():
-    user_list = []
-    print('Please type your list of items, separated by an enter:')
-    print('To duplicate an item, enter \'@dup\' as a following entry after you have entered the item')
-    print('To add a template list, enter \'@example\' as an item')
-    print('When done, enter \'@done\' as an item')
+# retrieve user data
+def collect(contents):
+    action_string = '@add @take @clear'
+    print('Enter \'@add, item, number\' to add \'number\' of \'item\' into the inventory')
+    print('Enter \'@take, item, number\' to take \'number\' of \'item\' from the inventory')
+    print('Enter \'@clear\' as an item to clear entire inventory')
 
     while True:
-        new = raw_input()
-        if new == '@done':
+        new = raw_input().split(', ')
+        # handle all invalid cases
+        if new[0] == '@clear':
+            contents = {}
             break
-        if new == '@example':
-            user_list = ['dollar bill', 'food', 'dollar bill', 'dollar bill', 'ruby']
-            break
-        if new == '':
-            print('You can\'t add an empty item value!')
-            continue
-        if len(new) > 72:
-            print('Character count too long.. Please enter a shorter item')
-            continue
-        if new == '@dup':
-            if len(user_list) == 0:
-                print('Only enter @dup after you have declared the item you want to duplicate')
-            else:
-                print('How many times would you like to duplicate?')
-                while True:
-                    print('Enter a multiplier:')
-                    mult = raw_input()
-                    if not mult.isdigit() or int(mult) < 1 or int(mult) > 100:
-                        print('Please only enter a positive integer from 1-100')
-                        continue
-                    break
-                for i in range(int(mult)):
-                    user_list.append(old)
+        elif len(new) != 3:
+            print('Unless using \'@clear\', three values must be entered')
+        elif new[0] not in action_string:
+            print('Invalid command entered')
+        elif not new[2].isdigit():
+            print('Number must contain digits only')
         else:
-          user_list.append(new)
-          old = new
-    return user_list
-    
+            # alter inventory
+            new[1] = new[1].title()
+            if new[0] == '@add':
+                contents = edit_item(contents, new, True) 
+            elif new[0] == '@take':
+                contents = edit_item(contents, new, False)
+            break
+    return contents
+
+# main function
 def main():
+    no_string = 'No no N n'
     print('Invent-a-story. Carve your own story! Enter items you would like to add to your inventory!')
-    print('Here\'s a start:')
-    contents = {'torch': 1, 'water': 6, 'dollar bill': 42, 'laptop': 1, 'food': 12}
-    contents = sort_inventory(contents)
+    if raw_input('Would you like to load a premade inventory? ') in no_string:
+        contents = {}
+    else:
+        print('Here\'s a start:')
+        contents = {'Torch': 1, 'Water': 6, 'Dollar Bill': 42, 'Laptop': 1, 'Food': 12}
+        contents = sort_inventory(contents)
     display_dictionary(contents, 'My inventory', True)
     while True:
-        add_list = collect()
-        new_items = normalise_list(add_list)
-        display_dictionary(new_items, 'Items collected', False)
-        contents = combine_inventory(contents, new_items )
+        contents = collect(contents)
         display_dictionary(contents, 'My inventory', True)
-        response_string = 'No no N n Exit exit Quit quit'
-        if raw_input('Add more? (Yes/No) ') in response_string:
+        if raw_input('Continue? (Yes/No) ') in no_string:
             break
 main()
